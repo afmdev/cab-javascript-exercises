7348db5ac120333cf24a0f68a5d1f888d8a59ac7
afm: alejandrofm.com <crixto@gmail.com>
2022-04-05 12:01:40 +0200

    New repository for JS excercises

---
+++ main.js
@@ -0,0 +1,71 @@
+/** 
+ * @ Author: Alejandro FM (alejanfm)
+ * @ Create Time: 2022-04-04 13:19:02
+ * @ Modified time: 2022-04-05 11:17:10
+ * @ Description: https://github.com/alejanfm
+ */
+
+
+//! JavaScript Basics Exercises
+
+console.log("Here start my JS excercises");
+
+//TODO  Writing expressions with variables
+
+//? EXCERCISE 1
+//* In your JavaScript file create a variable called myName with your name as the value. Put your name inside string quotes, e.g., "my name". Then add a line of code to print the variable name to the console after the previous message. Save your JavaScript file in your editor. Reload the HTML page in your browser. You should see your name printed. If not, investigate and fix it.
+
+let myName = "Ale"
+document.getElementById("myName").innerHTML = myName;
+console.log(myName);
+
+
+
+//? EXCERCISE 2
+//* Create a variable called age with a number that is your age. Do not use string quotes for numbers. Add a line to print that variable in the console. Save the file and reload the page. You should see your name and your age.
+
+let age = 36;
+document.getElementById("age").innerHTML = age;
+console.log(age);
+
+
+
+//? EXCERCISE 3
+//* Create a variable called juliaAge with a value 32. Create another variable called ageDiff and set it to an expression that calculates your age minus Julias's age. Print the value of ageDiff. Save the file and reload the page. You should see your age and the age difference. If you are younger than Julia, you should see a negative number.
+
+let juliaAge = 32
+let ageDiff = juliaAge - age
+document.getElementById("ageDiff").innerHTML = ageDiff;
+console.log(ageDiff);
+
+
+
+//TODO Writing code with conditionals:
+
+//? EXCERCISE 4
+//* Write a conditional that compares the variable with your age with the number 21. It should print either "You are older than 21" or "You are not older than 21", appropriately, depending on your age. Save your JavaScript file and reload the page. Make sure you see the correct message. Try changing your age in the JavaScript file to make sure the other message prints when it should.
+
+let number21 = 21
+
+if (age > number21) {
+    console.log("You are older than " + number21)
+} else {
+    console.log("You are not older than " + number21)
+}
+
+
+//? EXCERCISE 5
+//* Write a conditional that compares your age with Julia's age. This conditional will need to test if you are older, younger, or the same age, and print, appropriately, either "Julia is older than you", Julia is younger than you", or "You have the same age as Julia". Save your changes and reload the file.
+
+
+//! JavaScript Array: 
+
+//TODO Sorting an Array 
+
+//? EXCERCISE 1
+//* Create an array with all the names of your class (including mentors). Sort the array alphabetically. Then print the first element of the array in the console. On the next line, print the last element of the array in the console. On the next line, print all the elements of the array in the console (use a "for" loop). Save the file and reload the page. You should see the first element of the sorted array, the last element of the sorted array, and a list of all the elements in order in the array in the console.
+
+
+//? EXCERCISE 3
+
+
---
+++ script.js
@@ -0,0 +1,228 @@
+// console.log("Hello World");
+
+// // VARIABLES
+
+// var myVarVariable = 10
+// let myLetVariable = 20
+// const myConstVariable = 30
+
+
+// console.log(myVarVariable);
+// console.log(myLetVariable);
+// console.log(myConstVariable);
+
+
+// var sumOfNum = 1 + 2
+
+// console.log(sumOfNum)
+// console.log('Suma de numeros', sumOfNum)
+// console.log(myConstVariable + myVarVariable)
+
+// TYPES OF VARIABLES
+
+
+// let myNum = 1
+// let myName = "Alejandro FM"
+// let myBool = true
+// let myBoolFalse = false
+// let myArrayString = ["Hi", "there"]
+// let myArrayNum = [1, 2, 3, 4, 5]
+// let myArrayMix = [1, "ale", 2, "jandro", true]
+
+// console.log(myArrayMix)
+
+// console.log(myArrayMix[4])
+// console.log(myArrayMix.indexOf(true))  //Method
+
+// let quokkas = ["Angelina", "Arno", "Jess", "Karolina", "John", "Marta", "Ale"]
+
+// console.log("Quokkas team are", quokkas)
+// console.log(quokkas.indexOf("Karolina"))
+
+// quokkas.push("Lea")
+// console.log("Quokkas team are", quokkas)
+
+// quokkas.pop()
+// quokkas.reverse()
+// console.log("Quokkas team are", quokkas)
+
+
+// OBJECTS
+
+// let myMac = {
+//     keyboard: true,
+//     color: "silver",
+//     brand: "apple",
+//     screen: 15,
+//     ports: ["usb", "hdmi", "sd"]
+// }
+
+// let myWindows = {
+//     keyboard: true,
+//     color: "black",
+//     brand: "windows",
+//     screen: 15,
+//     ports: ["usb", "hdmi", "sd"]
+// }
+
+// console.log(myMac)
+// console.log(myWindows.color)
+
+// let myLaptops = []
+// myLaptops.push(myMac)
+// myLaptops.push(myWindows)
+// console.log(myLaptops)
+
+// ESTUDIAR QUE ES LET, VAR O CONST
+
+
+// COMPARISSON OPERATORS
+// ==, ===, !=, !==, <, >, <=, >=
+
+// CONDITIONAL STATEMENTS
+
+// IF
+
+// let myVar = 9
+// let myVar2 = 9
+// if (myVar == myVar2) {
+//     console.log("it is equal")
+// }
+
+
+// let myVar3 = 9
+// let myVar4 = "9"
+// if (myVar3 == myVar4) {
+//     console.log("it is equal")
+// }
+
+// if (1 < 2) {
+//     console.log("it is equal")
+// }
+
+// if (1 < 2) {
+//     console.log("it is equal")
+// }
+
+// let myNumber = 30
+
+// if (myNumber == 40) {
+//     console.log("you found the number")
+// } else {
+//     console.log("this is not my number")
+// }
+
+
+// let ale = 3
+
+// if (ale == 3) {
+//     console.log("you found the number")
+// } else if (ale > 4) {
+//     console.log("this is not my number")
+// } else if (ale < 2) {
+//     console.log("this is not my number")
+// } else {
+//     console.log("you downt know")
+// }
+
+
+// let quokkas = ["Angelina", "Arno", "Jess", "Karolina", "John", "Marta", "Ale"]
+// let sizeQuokkas = quokkas.length
+
+// for (let i = 0; i < sizeQuokkas; i = i + 1) {
+//     console.log("For " + quokkas[i] + ",aaaaaaaaa" + " the position is " + i)
+// }
+
+
+// // WHILE LOOP
+
+// let x= 0;
+// while (x < 5) {
+//     console.log(x);
+//     x++
+// }
+
+
+//
+// JAVA SCRIPT FUNCTIONS
+//
+
+// function greetFunction() {
+//     console.log("Hi There");
+// }
+
+
+// greetFunction()
+
+
+// function greet2() {
+//     console.log("Hi there, in the morning");
+    
+// }
+
+
+
+// function greet3(msg) {
+//     console.log(msg);
+    
+// }
+
+// greet3("Ale")
+// greet3("jandro")
+// greet3("Francisco")
+// greet3(56)
+
+
+// function todaysMenu(first, second) {
+// console.log("Today's menu is " +first+ " and" + second);    
+// }
+
+// todaysMenu("paella", "sopa")
+// todaysMenu("ensalada", "crema")
+// todaysMenu("lasagna", "pasta")
+// todaysMenu("ice cream", "arroz")
+
+// let first = "caviar"
+// let second = "sopa"
+// todaysMenu(second, first)
+
+
+function sumOfTwoNumbers(a, b) {
+    let result = a + b;
+    // console.log(result)
+        return result
+}
+
+let myResult = sumOfTwoNumbers(10, 55)
+    console.log(myResult)
+
+
+
+function costOfMenu(mainPrice, dessertPrice) {
+    let result = mainPrice + dessertPrice;
+    return result;
+}
+
+costOfMenu(10, 7)
+
+let menuPrice = costOfMenu(10, 7)
+console.log(menuPrice);
+
+function restaurantBill(menuPrice) {
+    let myMoney = 15
+
+    if (menuPrice > myMoney) {
+        console.log("No money");
+    } else {
+        console.log("Take my money and run");
+    }
+}
+
+restaurantBill(menuPrice)
+
+
+
+
+// let myFunction = (parameter) => {
+//     //my code
+// }

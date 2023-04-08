4cd6d0142b7a8b2088eb9459ee79a6f87090e262
Alejandro FM <102042032+alejanfm@users.noreply.github.com>
2023-04-09 00:00:43 +0200

    Refreshing

--- main3.js
+++ main3.js
@@ -13,17 +13,48 @@ console.log("Ex 2: " + myAge)
 
 //3
 let juliaAge = 50
-let aleAge = 37
+let aleAge = 39
 let ageDiff = juliaAge - aleAge
 document.getElementById("ageDiff").innerHTML = ageDiff;
 console.log("Ex3: age diff " + ageDiff)
 
 //4
 let theAge = 21
-let myAge2 = 33
-
-if (theAge < myAge2) {
-	console.log("Your are older than 21")
+let queloque = ""
+if (theAge < myAge) {
+	let queloque = "older"
+	document.getElementById("queloque").innerHTML = queloque
+	console.log("4: You are " + queloque + " than 21")
 } else {
-	console.log("Your are youngest than 21")
+	let queloque = "younger"
+	document.getElementById("queloque").innerHTML = queloque
+	console.log("4: You are " + queloque + " than 21")
+}
+
+//5
+if (juliaAge == myAge) {
+	document.getElementById("compareJulia").innerHTML = "You have same age as Julia"
+	console.log("5: You have same age as Julia")
+} else if (juliaAge < myAge) {
+	document.getElementById("compareJulia").innerHTML = "You are older than Julia"
+	console.log("5: You are older than Julia")
+} else if (juliaAge > myAge) {
+	document.getElementById("compareJulia").innerHTML = "You are younger than Julia"
+	console.log("5: You are younger than Julia")
+}
+
+//6
+let family = ["ale", "ceci", "rosita", "odam", "lester", "panchoti"]
+
+let familyLength = family.length
+
+document.getElementById("alphabetically").innerHTML = "Sort the array alphabetically: " + family.sort()
+document.getElementById("firstElement").innerHTML = "First element array: " + family[0]
+document.getElementById("lastElement").innerHTML = "Last element array: " + family[family.length - 1]
+
+for (let i = 0; i < family.length; i++) {
+	document.getElementById("loopElements").innerHTML = i + " " + family[i]
+	// document.getElementById("loopElements").innerHTML = family.slice(-1);
+	console.log("6 Loop: " + family[i])
+
 }
